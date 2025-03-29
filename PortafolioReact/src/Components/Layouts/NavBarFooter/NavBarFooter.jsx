41b7bcadb910aff8998b0399e077a43d5a571b9a
jllxno <juanllxno201@gmail.com>
2025-03-28 21:15:02 -0500

    Add Links

--- PortafolioReact/src/Components/Layouts/NavBarFooter/NavBarFooter.jsx
+++ PortafolioReact/src/Components/Layouts/NavBarFooter/NavBarFooter.jsx
@@ -5,11 +5,47 @@ export const NavBarFooter = () => {
   return (
     <nav>
       <ul className='List-footer'>
-        <ItemNavbarFooter iconName="facebook-square" iconType="logo" iconAnimation="tada" iconClass="icon-facebook" color="#ffffff" />
-        <ItemNavbarFooter iconName="twitter" iconType="logo" iconAnimation="tada" iconClass="icon-twitter" iconColor="#1da1f2" />
-        <ItemNavbarFooter iconName="instagram" iconType="logo" iconAnimation="tada" iconClass="icon-instagram" iconColor="#e1306c" />
-        <ItemNavbarFooter iconName="linkedin-square" iconType="logo" iconAnimation="tada" iconClass="icon-linkedin" iconColor="#0077b5" />
+        <li>
+          <ItemNavbarFooter
+            href="https://www.facebook.com/?locale=es_LA"
+            iconName="facebook-square"
+            iconType="logo"
+            iconAnimation="tada"
+            iconClass="icon-facebook"
+            iconColor="#ffffff"
+          />
+        </li>
+        <li>
+          <ItemNavbarFooter
+            href="https://x.com/?lang=es"
+            iconName="twitter"
+            iconType="logo"
+            iconAnimation="tada"
+            iconClass="icon-twitter"
+            iconColor="#1da1f2"
+          />
+        </li>
+        <li>
+          <ItemNavbarFooter
+            href="https://www.instagram.com/"
+            iconName="instagram"
+            iconType="logo"
+            iconAnimation="tada"
+            iconClass="icon-instagram"
+            iconColor="#e1306c"
+          />
+        </li>
+        <li>
+          <ItemNavbarFooter
+            href="https://github.com/santimiranda791"
+            iconName="github"
+            iconType="logo"
+            iconAnimation="tada"
+            iconClass="icon-linkedin"
+            iconColor="#0077b5"
+          />
+        </li>
       </ul>
     </nav>
   );
-}
+};
--- PortafolioReact/src/Components/Ui/ItemNavbarFooter/ItemNavbarFooter.jsx
+++ PortafolioReact/src/Components/Ui/ItemNavbarFooter/ItemNavbarFooter.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import 'boxicons';
 
-export const ItemNavbarFooter = ({ iconName, iconType, iconAnimation, iconClass, iconColor }) => {
+export const ItemNavbarFooter = ({ href, iconName, iconType, iconAnimation, iconClass, iconColor }) => {
   return (
-    <li>
-      <a href="">
-        <box-icon 
-          name={iconName} 
-          type={iconType} 
-          animation={iconAnimation} 
-          class={iconClass} 
-          style={{ color: iconColor }}
-        ></box-icon>
-      </a>
-    </li>
+    <a href={href} target="_blank" rel="noopener noreferrer" className="item-navbar-footer">
+      <box-icon
+        name={iconName}
+        type={iconType}
+        animation={iconAnimation}
+        class={iconClass}
+        style={{ color: iconColor }}
+      ></box-icon>
+    </a>
   );
-}
+};
